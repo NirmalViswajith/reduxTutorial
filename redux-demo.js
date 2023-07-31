24cf1a18187f535174c654c1c27c6255f389d445
NirmalViswajith <nikanomi06@gmail.com>
2023-07-31 20:51:56 +0530

    decremented

--- redux-demo.js
+++ redux-demo.js
@@ -1,1 +1,26 @@
-console.log('helloworld')
+const redux = require('redux');
+
+const counterReducer = (state = {counter: 0}, action) => {
+ if(action.type === 'increment'){
+  return {
+    counter: state.counter + 1
+  }
+ } else if (action.type === 'decrement'){
+  return {
+    counter: state.counter - 1
+  }
+ }
+}
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const state = store.getState();
+  console.log(state);
+}
+
+store.subscribe(counterSubscriber);
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
